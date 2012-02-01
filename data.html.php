18674bdc093ee437b44176cf3be0b549b048fee0
jonathantneal <jonathantneal@hotmail.com>
2012-02-01 14:45:56 -0800

    Beautified HTML/CSS

--- data.html.php
+++ data.html.php
@@ -10,27 +10,41 @@ foreach ($features as &$featureName) {
 	array_push($featuresNames, $featureTitle);
 }
 
-foreach ($jsonArrNew['required'] as $browserName => &$browserVersion) {
-	$browserHtml = $jsonArrAgs[$browserName]['browser'] . ' ' . $browserVersion;
-	$browserHtml = '<a href="' . $jsonArrAgs[$browserName]['url'] . '" rel="external" target="_blank">' . $browserHtml . '</a>';
+foreach ($jsonArrNew['required'] as $browserId => &$browserVersion) {
+	$browserName = $jsonArrAgs[$browserId]['browser'];
+	$browserUrl  = $jsonArrAgs[$browserId]['url'];
+	$browserFull = $browserName . ' ' . $browserVersion;
+
+	$browserIcon = '<img alt="' . $browserName . '" src="i/' . $browserId . '-128.png">';
+	$browserHtml = '<a href="' . $browserUrl . '" rel="external" target="_blank">' . $browserIcon . '<br><span>' . $browserFull . '</span></a>';
 
 	array_push($featuresBrowsers, $browserHtml);
 }
 
 $featuresNamesHtml = implode(' &amp; ', $featuresNames);
-$featuresBrowsersHtml = implode(' &amp; ', $featuresBrowsers);
+$featuresBrowsersHtml = implode('', $featuresBrowsers);
 
 ?>
 <!doctype html>
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Can I use <?php print($featuresNamesHtml); ?>?</title>
+		<title>Which browsers can use <?php print($featuresNamesHtml); ?>?</title>
+		<style>
+		* { vertical-align: top; }
+		html { background: #444; color: #EEE; font: 100%/1 sans-serif; }
+		body { text-align: center; }
+		h1 { font-size: 2em; margin: 1em; }
+		a { color: #CB0; display: inline-block; margin: 1em; text-align: center; text-decoration: none; -webkit-transition: all 200ms ease-out; -webkit-transform: scale(0.9); width: 132px; white-space: nowrap; }
+		a:focus, a:hover { color: #CF0; -webkit-transform: scale(0.9999999); }
+		a img { border: 0; height: 128px; margin: 0 0 0.5em; width: 128px; }
+		a span { clear: both; }
+		</style>
 	</head>
 	<body>
-		<h1>Can I use <?php print($featuresNamesHtml); ?>?</h1>
+		<h1><?php print($featuresNamesHtml); ?></h1>
 		<p>
-			Yes, in <?php print($featuresBrowsersHtml); ?>.
+			<?php print($featuresBrowsersHtml); ?>
 		</p>
 	</body>
 </html>
