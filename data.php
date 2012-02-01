3fddb32e20ee0e6bae6c60fa0bf3b7b0f9d7bfc8
jonathantneal <jonathantneal@hotmail.com>
2012-01-31 22:43:52 -0800

    Optimizations

--- data.php
+++ data.php
@@ -1,21 +1,14 @@
 <?php
 
 $callback = isset($_GET['callback']) ? $_GET['callback'] : null;
-$type = isset($_GET['type']) ? $_GET['type'] : 'js';
+$type     = isset($_GET['type']) ? $_GET['type'] : 'js';
 $features = isset($_GET['features']) ? explode(' ', $_GET['features']) : array();
 
-if ($type === 'json')
-{
-	header('Content-Type: text/json');
-}
+$header = 'Content-Type: ' . ( $type === 'js' ? 'text/javascript' : ( $type === 'json' ? 'text/json' : 'text/html' ) );
 
-if ($type === 'js')
-{
-	header('Content-Type: text/javascript');
-}
+header($header);
 
-if (!$features)
-{
+if (!$features) {
 	exit();
 }
 
@@ -25,27 +18,24 @@ $jsonText = file_get_contents($jsonName);
 
 $jsonData = json_decode($jsonText, true);
 
-$jsonDataData = $jsonData['data'];
+$jsonDataData =& $jsonData['data'];
 
 $jsonDataDataCustom = array();
 
-foreach ($features as $featureName)
-{
-	if (isset($jsonDataData[$featureName]))
-	{
-		$featureData = $jsonDataData[$featureName];
+foreach ($features as &$featureName) {
+	if (isset($jsonDataData[$featureName])) {
+		$featureData  = $jsonDataData[$featureName];
 		$featureStats = $featureData['stats'];
 
 		$jsonDataDataCustom[$featureName] = $featureData;
 	}
 }
 
-if ($callback)
-{
+if ($type === 'js' && $callback) {
 	exit($callback . '(' . json_encode($jsonDataDataCustom) . ')');
 }
-else
-{
+
+if ($type === 'json') {
 	exit(json_encode($jsonDataDataCustom));
 }
 
