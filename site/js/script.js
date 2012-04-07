15c54d552f2c6978d13663ec85e5d721a6349230
Hans Christian Reinl <anything@drublic.de>
2012-04-08 01:18:48 +0200

    Remove call for initApp() in Modernizr plugin, as it causes an exception

--- site/js/script.js
+++ site/js/script.js
@@ -26,7 +26,7 @@
            preprefix: '&lt;div id="h5p-message">&lt;/div>\n&lt;script async>',
            plugin: undefined,
            prefix : '\n\nModernizr.html5please({ \n  features: ',
-           suffix: ', \n  yep: function(){ initApp() /* replace this by your own init */ }, // all tests pass. initialize app. \n  nope: function(a){ document.getElementById("h5p-message").innerHTML=a.html; }\n})&lt;/script>',
+           suffix: ', \n  yep: function(){ /* put your own initApp() here */ }, // all tests pass. initialize app. \n  nope: function(a){ document.getElementById("h5p-message").innerHTML=a.html; }\n})&lt;/script>',
            message: 'Make sure you include <a href="http://modernizr.com">modernizr</a> inside the head tag of your markup'
           };
 
